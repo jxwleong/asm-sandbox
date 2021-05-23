34cf11ad50eee90e579bac77789d010d6f02c04f
jxwleong <jason.xie.wei.leong@gmail.com>
2021-05-23 13:01:21 +0800

    Add few procedures from https://www.youtube.com/watch?v=p5X1Sf5ejCc Next AR: Add some comments on asm.asm and figure out why the logical processor count show 16 instead of 8.

--- asm.asm
+++ asm.asm
@@ -1,6 +1,58 @@
+.data
+vendorString db 12 dup(0), 0
+
 .code
 SomeFunction proc
 	mov rax, 123
 	ret
 SomeFunction endp
+
+GetCPUIDSupport proc
+	push rbx
+	pushfq
+	pop rax
+	mov rbx, rax
+	xor rax, 200000h
+	push rax
+	popfq
+	pushfq
+	pop rax
+	cmp  rax, rbx
+
+	jz No_CPUID
+	pop  rbx
+	mov  rax, 1
+	ret
+
+No_CPUID:
+	pop rbx
+	mov rax, 0
+	ret
+GetCPUIDSupport endp
+
+GetVendorString proc
+	push rbx
+	mov eax, 0
+	cpuid
+	lea rax, vendorString
+
+	mov dword ptr [rax], ebx
+	mov  dword ptr [rax+4], edx
+	mov dword ptr[rax+8], ecx
+
+	pop rbx
+	ret
+	ret
+GetVendorString endp
+
+GetLogicalProcessorCount proc
+	push rbx
+	mov eax, 1
+	cpuid
+	and ebx, 00ff0000h
+	mov eax, ebx
+	pop rbx
+	shr eax, 16
+	ret
+GetLogicalProcessorCount endp
 end
--- main.cpp
+++ main.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 extern "C" int SomeFunction();
+extern "C" bool GetCPUIDSupport();
+extern "C" char* GetVendorString();
+extern "C" int GetLogicalProcessorCount();
+
+string getBoolStr(bool boolean) {
+	if (boolean == 1)
+		return "true";
+	else
+		return "false";
+}
+
 
 int main() {
 	cout << "The result is: " << SomeFunction() << endl;
-
+	cout << "Support CPUID Instruction?: " << getBoolStr(GetCPUIDSupport()) << endl;
+	cout << "Vendor String: " << GetVendorString() << endl;
+	// 16 Core??
+	// Have to check with datasheet...
+	cout << "Logical Processor Count: " << GetLogicalProcessorCount() << endl;
 	return 0;
 }
